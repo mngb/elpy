b48260df51aaa9a63da0f32023ad2409f8cd7f5f
pk <Kevin.remegame@gmail.com>
2024-09-29 13:43:46 +0800

    [fix] conflict with local company configure

--- elpy-pkg.el
+++ elpy-pkg.el
@@ -1,7 +1,7 @@
 (define-package "elpy" "1.35.0"
                 "Emacs Python Development Environment"
-                '((company "0.9.2")
-                  (emacs "24.4")
+                '(;;(company "0.9.2") ;;conflict with local company
+                  ;;(emacs "24.4")
                   (highlight-indentation "0.5.0")
                   (pyvenv "1.3")
                   (yasnippet "0.8.0")
